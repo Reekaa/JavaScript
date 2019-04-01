2d8df351cd19db399739bae664575d89a13b7de5
Reka <a9886177@9886177s-MacBook-Pro.local>
2019-04-01 14:44:22 +0100

    JavaScript_recap

---
+++ app.js
@@ -0,0 +1,36 @@
+// Data types and variables
+var myName = "Reka";
+var myAge = 31;
+var mySurname = "Forgacs";
+console.log(myName + " " + mySurname);
+// Conditinals
+var pinNumber = 1234;
+if(pinNumber === 1234){
+  console.log("correct pin number");
+}else{
+  console.log("incorrect!");
+}
+// Loops
+var animal = ['cat', 'donkey', 'goose']
+
+for (var i = 0; i < animal.length; i++) {
+ console.log(animal[i]);
+}
+// Funcations
+var printName = function(){
+  console.log("Reka");
+}
+printName ();
+printName ();
+printName ();
+printName ();
+// Arrays
+var pubNames = ['Cloisters', '4042', 'The Red Lion'];
+console.log(pubNames);
+// Objects
+var person = {
+  name: "Reka",
+  pinNumber:1234,
+  tel: 12345,
+}
+console.log(person);
